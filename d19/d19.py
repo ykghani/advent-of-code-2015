c8b8bba4f9823dca632848d74f290a94ad69f4cf
ykghani <yusuf@chicagobooth.edu>
2025-01-12 21:05:59 -0500

    part 2 solved

--- d19/d19.py
+++ d19/d19.py
@@ -2,7 +2,7 @@
 Advent of Code 2015 Day 19 - Medicine for Rudolph
 '''
 from pathlib import Path
-import itertools
+from collections import deque 
 
 script_dir = Path(__file__).parent
 file_path = script_dir / 'd19_input.txt'
@@ -22,14 +22,7 @@ for line in lines[:-1]:
 
 inp = lines[-1].strip()
 
-print(replacements)
-
-# replacements = {'H' : ['HO', 'OH'], 'O' : ['HH']}
-
 unique = set()
-# inp = 'HOHOHO'
-
-
 for start in range(len(inp)):
     for length in range(len(inp) - start + 1):
         # out = inp[: start]
@@ -40,3 +33,39 @@ for start in range(len(inp)):
                 unique.add(out)
 
 print(f"Part one: {len(unique)}")
+
+def reverse_replacements(replacements):
+    '''Reverses the replacement dictionary'''
+    
+    reverse_dict = {}
+    for key, values in replacements.items():
+        for val in values: 
+            if val in reverse_dict:
+                reverse_dict[val].append(key)
+            else:
+                reverse_dict[val] = [key]
+    
+    return dict(sorted(reverse_dict.items(), key= lambda x: len(x[0]), reverse= True))
+
+def find_min_steps(target, replacements):
+    reverse_rep = reverse_replacements(replacements)
+
+    queue = [(target, 0)]
+    seen = {target}
+    while queue:
+        current, steps = queue.pop()
+        
+        if current == 'e':
+            return steps
+
+        for start in range(len(current)):
+            for length in range(1, len(current) - start + 1):
+                substr = current[start: start + length]
+                if substr in reverse_rep:
+                    for rep in reverse_rep[substr]:
+                        out =  current[: start] + rep + current[start + length:]
+                        seen.add(out)
+                        queue.append((out, steps + 1))
+    return None
+
+print(f"Part two: {find_min_steps(inp, replacements)}")
